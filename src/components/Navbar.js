0c2d2af97e3d906d42be7b6fa4dbdab764a88e39
Vanshul <vanshuldahiya2002@gmail.com>
2023-02-12 23:41:00 +0530

    added starter files and dependencies

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <div>
+      <FaBars />
+    </div>
+  );
+};
+
+export default Navbar;
